c045dbdd7b7014fd197346d7a956286e9008eb92
Kyle Rumble <kylerumble@gmail.com>
2020-05-01 10:07:45 -0700

    Download live theme files

--- wp-content/themes/_studiozerbey/partials/project_preview_box.php
+++
@@ -1,8 +0,0 @@
-<div class="project-preview-box">
-    <a href="<?php the_permalink(); ?>">
-        <?php the_post_thumbnail(array(463, 287)); ?>
-        <div class="title-banner">
-            <h3><?php the_title(); ?></h3>
-        </div>
-    </a>
-</div>
